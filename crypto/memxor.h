1ad74b84b5b1eacb5b029e9799ff9bef437ead32
flabbergast <s3+flabbergast@sdfeu.org>
2014-10-28 14:25:08 +0000

    Commit avr-crypto-lib files directly.
    Saves *a lot* of space compared to submoduling.

--- crypto/memxor.h
+++ crypto/memxor.h
@@ -1,1 +1,9 @@
-avr-crypto-lib/memxor/memxor.h
+#ifndef MEMXOR_H_
+#define MEMXOR_H_
+#include <stdint.h>
+
+void memxor(void *dest, const void *src, uint16_t n);
+void memxor_P(void *dest, const void *src, uint16_t n);
+
+
+#endif
